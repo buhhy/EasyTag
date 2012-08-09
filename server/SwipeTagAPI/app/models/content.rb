ed39cf28224cc90d5b19e75d1243fd6675368350
Steve McQuaid <steve@stevemcquaid.com>
2012-08-08 21:34:00 -0700

    edited models

--- server/SwipeTagAPI/app/models/content.rb
+++ server/SwipeTagAPI/app/models/content.rb
@@ -1,2 +1,7 @@
 class Content < ActiveRecord::Base
+  has_many :tags, :through => :content_tag
+  
+  scope :alphabetical, order("name")
+  scope :for_tag,   lambda { |tag_id| where("tag_id = ?", tag_id) }
+  
 end
--- server/SwipeTagAPI/app/models/tag.rb
+++ server/SwipeTagAPI/app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ActiveRecord::Base
+  has_many :contents, :through => :content_tags
+  
+ scope :alphabetical, order("name")
+ scope :for_content, lambda { |content_id| where("content_id = ?", content_id) }
+ 
 end
